cd4d567af262c0a95d7b6232790a8f662c9f83ca
zengkang <android_zengkang@163.com>
2019-03-05 11:30:18 +0800

    Initial commit

---
+++ mytestlibrary/src/main/java/com/example/mytestlibrary/Utils.java
@@ -0,0 +1,10 @@
+package com.example.mytestlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Utils {
+    public void showToast(Context context){
+        Toast.makeText(context,"哈哈哈哈",Toast.LENGTH_SHORT).show();
+    }
+}
